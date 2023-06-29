cbbb0e4f1d3f710ee6dd3ab589c9c4719f2ebd0a
Himanshu Kushwaha <124574943+Web-Dev-Kush@users.noreply.github.com>
2023-06-29 23:01:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import WatchPage from "./components/WatchPage";
+import ErrorPage from "./components/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
